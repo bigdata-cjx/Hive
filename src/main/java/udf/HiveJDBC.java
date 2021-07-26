d4f6efaadfceac729c54f98fe88dea88ae970f68
cjx <1067446576@qq.com>
2021-07-26 22:56:36 +0800

    HiveJDBC

---
+++ src/main/java/udf/HiveJDBC.java
@@ -0,0 +1,36 @@
+package udf;
+
+import java.sql.*;
+
+public class HiveJDBC {
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+    /**
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(String[] args) throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
+        }
+        //replace "hive" here with the name of the user the queries should run as
+        Connection con = DriverManager.getConnection("jdbc:hive2://bigdata:10000/db_hive",
+                "root", "123456");
+        Statement stmt = con.createStatement();
+        String tableName = "order";
+
+        ResultSet res = null;
+
+        // select * query
+        String sql = "select * from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
+        }
+    }
+}
